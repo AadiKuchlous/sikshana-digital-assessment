89965c370a664993b124e06dcecf293952b645c4
Aadi Kuchlous <aadikuchlous@gmail.com>
2022-12-29 19:33:56 +0530

    reading excel

--- main.js
+++ main.js
@@ -1,3 +1,19 @@
 function main() {
   return;
 }
+
+function readExcelSheet(file) {
+  readXlsxFile(file).then(function(rows) {
+    console.log(rows)
+    // `rows` is an array of rows
+    // each row being an array of cells.
+  })
+}
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  var input = document.getElementById('input')
+  input.addEventListener('change', function() {
+    readExcelSheet(input.files[0])
+  })
+})
